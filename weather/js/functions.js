92b8c80477c95591598527e1c9350c6e0e678ce2
wiseman7 <wis16002@byui.edu>
2019-02-26 17:58:53 -0700

    wind chill function

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -1,4 +1,39 @@
 /*****************************
  * Weather Site JavaScript Functions
  **************************************/
-console.log('My javascript is being read.');
+
+    // Variables for Function Use
+    const temp = 31;
+    const speed = 5;
+
+    buildWC(speed, temp);
+
+
+
+ // calculate wind chill temperature.
+ function buildWC(speed, temp){
+  
+const feelTemp= document.getElementById('feelTemp');
+
+// compute the windchill
+let wc = 35.74 + 0.615 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+console.log(wc);
+
+// Round the answer down to integer
+wc = Math.floor(wc);
+
+// If chill is greater than temp, return the temp
+wc = (wc > temp)?temp:wc;
+
+// Display windchill
+console.log(wc);
+
+// wc = 'Feels like '+wc+' F;
+wc = 'Feels like ' + wc + '&deg;F';
+feelTemp.innerHTML= wc;
+
+ }
+
+
+
+ 
