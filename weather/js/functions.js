3d83eefa0101cc7ad924d6d46a2586978dbdc71b
wiseman7 <wis16002@byui.edu>
2019-02-26 20:20:13 -0700

    getCondition

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -3,27 +3,30 @@
  **************************************/
 
     // Variables for Function Use
-    const temp = 31;
+    const curtemp = 31;
     const speed = 5;
 
-    buildWC(speed, temp);
+    buildWC(speed, curtemp);
+
+    const direction = "NNE";
+    windDial(direction);
 
 
 
  // calculate wind chill temperature.
- function buildWC(speed, temp){
+ function buildWC(speed, curtemp){
   
 const feelTemp= document.getElementById('feelTemp');
 
 // compute the windchill
-let wc = 35.74 + 0.615 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+let wc = 35.74 + 0.615 * curtemp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * curtemp * Math.pow(speed, 0.16);
 console.log(wc);
 
 // Round the answer down to integer
 wc = Math.floor(wc);
 
 // If chill is greater than temp, return the temp
-wc = (wc > temp)?temp:wc;
+wc = (wc > curtemp)?curtemp:wc;
 
 // Display windchill
 console.log(wc);
@@ -32,8 +35,66 @@ console.log(wc);
 wc = 'Feels like ' + wc + '&deg;F';
 feelTemp.innerHTML= wc;
 
- }
+}
 
+// Wind Dial Function
+function windDial(direction){
+    const windimg = document.getElementById("windimg");
+    console.log(direction)
 
+    // Determine the dial class
+    switch (direction){
+        case "North":
+        case "N":
+          windimg.setAttribute("class", "n");
+          break;
+        case "NE":
+        case "NNE":
+        case "ENE":
+          windimg.setAttribute("class", "ne");
+          break;
+        case "NW":
+        case "NNW":
+        case "WNW":
+          windimg.setAttribute("class", "nw");
+          break;
+        case "South":
+        case "S":
+          windimg.setAttribute("class", "s");
+          break;
+        case "SE":
+        case "SSE":
+        case "ESE":
+          windimg.setAttribute("class", "se");
+          break;
+        case "SW":
+        case "SSW":
+        case "WSW":
+          windimg.setAttribute("class", "sw");
+          break;
+        case "East":
+        case "E":
+          windimg.setAttribute("class", "e");
+          break;
+        case "West":
+        case "W":
+          windimg.setAttribute("class", "w");
+          break;
+    }
+}
+
+function getCondition(phrase){
+    if (phrase == "clear")
+    {
+
+    }
+}
+
+function convertMeters(meters){
+    const feet = 3.28 * meters;
+    const round = Math.round(feet);
+
+    document.getElementById("convertMeters") = round;
+}
 
  
