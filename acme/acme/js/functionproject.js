ba85bafbefeebb73681bf4f1929d1ec4d78beef8
wiseman7 <wis16002@byui.edu>
2019-04-01 13:00:11 -0600

    final

--- acme/acme/js/functionproject.js
+++ acme/acme/js/functionproject.js
@@ -1,6 +1,11 @@
-
 'use strict';
 
+document.querySelector("body").addEventListener('click', function (e) {
+    var anchor = e.target.closest('a');
+    if (anchor !== null) {
+        console.log(anchor.textContent);
+    }
+}, false);
 
 /*************************JSON************************************* */
 
@@ -17,12 +22,32 @@ function fetchData(acmeURL) {
         })
         .then(function (data) {
             console.log(data);
+           
+           
 
         })
 
-        
-// If there is an error
-.catch(function (error) {
-    console.log('There was a fetch problem: ', error.message);
-})
+        // Json
+
+        // let product-name = ____.name;
+        // let product-image=__________.path;
+        // let product-description = ________.description;
+        // let manufacturer = _________________.manufacturer;
+        // let reviews = ____________________.reviews;
+        // let price = ______________.price;
+
+
+        /* HTML */
+
+        // document.getElementById('product-name').innerHTML = product-name;
+        // document.getElementById('product-image').innerHTML = product-image;
+        // document.getElementById('manufacturer').innerHTML = manufacturer;
+        // document.getElementById('reviews').innerHTML = reviews;
+        // document.getElementById('price').innerHTML = price;
+
+
+        // If there is an error
+        .catch(function (error) {
+            console.log('There was a fetch problem: ', error.message);
+        })
 }
